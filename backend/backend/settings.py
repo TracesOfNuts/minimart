28e4243f22e1e6dedb9f8c7412cc926c7b548ad7
tracesofnuts <ahmadzahiri90@gmail.com>
2023-12-18 23:05:09 +0800

    Enable CORS for all origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,8 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000',)
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'backend.urls'
 
