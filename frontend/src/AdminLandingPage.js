be59773b31ab3653a437debb86e4e497e9036a09
tracesofnuts <ahmadzahiri90@gmail.com>
2023-12-18 21:07:16 +0800

    Add AdminLandingPage component with product display

---
+++ frontend/src/AdminLandingPage.js
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { 
+  Card, 
+  CardActionArea,
+  CardContent, 
+  CardMedia, 
+  Container, 
+  CssBaseline, 
+  Grid, 
+  Paper, 
+  Typography 
+  } from "@mui/material";
+import AppAppBar from "./components/AppAppBar";
+import banner from './banner.jpg';
+
+function AdminLandingPage() {
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/products");
+      const products = await response.json();
+      setProducts(products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <AppAppBar title="Welcome to Jack's Minimart" />
+        <Paper 
+          elevation={3} 
+          sx={{ 
+            height:550,
+            position:'relative', 
+            my: 10, 
+            backgroundImage: `url(${banner})`, 
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat', 
+          }}
+        >
+        </Paper>
+        <Grid container spacing={4}>
+          {products.map((product) => (
+            <Grid item key={product.id} xs={12} sm={6} md={4}>
+              <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+                <CardActionArea component={Link} to={`/products/${product.id}`}>
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: "56.25%",
+                    }}
+                    image={product.image}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {product.name}
+                    </Typography>
+                    <Typography>
+                      {product.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default AdminLandingPage;
