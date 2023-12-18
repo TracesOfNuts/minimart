c7b594b4c4f60b19617a4b64d796f587f19ac553
tracesofnuts <ahmadzahiri90@gmail.com>
2023-12-18 18:38:19 +0800

    Refactor App.js and add banner image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Container, CssBaseline, Paper } from "@mui/material";
+import AppAppBar from "./components/AppAppBar";
+import banner from './banner.jpg';
 
 function App() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = React.useState([]);
 
-  useEffect(() => {
+  React.useEffect(() => {
     fetchProducts();
   }, []);
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/products');
+      const response = await fetch("http://127.0.0.1:8000/api/products");
       const products = await response.json();
       setProducts(products);
     } catch (error) {
-      console.error('Error fetching products:', error);
+      console.error("Error fetching products:", error);
     }
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Jack's Minimart!
-        </p>
-      </header>
-      <div className="product-list">
-          {products.map(product => (
-            <div className="product" key={product.id}>
-              <h3>{product.name}</h3>
-              <p>{product.description}</p>
-              <p>Price: ${product.price}</p>
-            </div>
-          ))}
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <AppAppBar title="Welcome to Jack's Minimart" />
+        <Paper 
+          elevation={3} 
+          sx={{ 
+            height:550,
+            position:'relative', 
+            marginTop: 10, 
+            backgroundImage: `url(${banner})`, 
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat', 
+          }}
+        >
+          {/* {<img style={{ display: 'none'}} src={banner} alt="banner" />} */}
+        </Paper>
+
+
+
+        <div className="App">
+          <div className="product-list">
+            {products.map((product) => (
+              <div className="product" key={product.id}>
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <p>Price: ${product.price}</p>
+              </div>
+            ))}
+          </div>
         </div>
-    </div>
+      </Container>
+    </React.Fragment>
   );
 }
 
