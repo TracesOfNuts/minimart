3e7b1ff4b5aedd9c3362d4afbf5fd8ddc1292d0d
tracesofnuts <ahmadzahiri90@gmail.com>
2023-12-18 21:07:29 +0800

    Add react-router-dom for admin pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import AdminLandingPage from './AdminLandingPage';
+import AdminProductPage from './AdminProductPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AdminLandingPage />,
+  },
+  {
+    path: '/products/:id',
+    element: <AdminProductPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
