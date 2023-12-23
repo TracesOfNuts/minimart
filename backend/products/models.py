9522dda4e382ed28df03fa06d1b5c8a8bff745e5
Ahmad Zahiri <ahmadzahiri90@gmail.com>
2023-12-23 11:15:47 +0800

    Refactor image field in Product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
