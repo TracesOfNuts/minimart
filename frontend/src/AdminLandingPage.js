56b7b7f651acea4cf8dc1c998778f804c8fe41fd
Ahmad Zahiri <ahmadzahiri90@gmail.com>
2023-12-23 18:34:26 +0800

    Update product image in AdminLandingPage.js

--- frontend/src/AdminLandingPage.js
+++ frontend/src/AdminLandingPage.js
@@ -111,7 +111,7 @@ function AdminLandingPage() {
                       // 16:9
                       pt: "56.25%",
                     }}
-                    image='https://source.unsplash.com/random?fruit,vegetable'
+                    image={product.image}
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
